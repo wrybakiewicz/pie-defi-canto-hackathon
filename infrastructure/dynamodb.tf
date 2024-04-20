f32c2c4474dabc9f46598916f0f89a5e36a92999
Wojtek Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-04-20 17:31:31 +0200

    add last sync block table

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -1,17 +1,17 @@
-resource "aws_dynamodb_table" "this" {
-  name         = "piedefi"
+resource "aws_dynamodb_table" "last_synced_block_v1" {
+  name         = "piedefi-last-synced-block-v1"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "hash"
-  range_key    = "range"
+  hash_key     = "partition"
+  range_key    = "blockNumber"
 
   attribute {
-    name = "hash"
+    name = "partition"
     type = "S"
   }
 
   attribute {
-    name = "range"
-    type = "S"
+    name = "blockNumber"
+    type = "N"
   }
 
   provider = aws.eu-west-2
