2faa5d654b004467a33e77d6b26145321fe09477
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 20:37:09 +0200

    Add timeline

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ComponentsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
