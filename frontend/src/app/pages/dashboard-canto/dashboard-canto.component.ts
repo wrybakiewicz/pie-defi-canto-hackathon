cb5ad0bf95885a87e3aea792bb7b9cbb712cc51e
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 16:52:17 +0200

    Add cadence dashboard

--- frontend/src/app/pages/dashboard-canto/dashboard-canto.component.ts
+++ frontend/src/app/pages/dashboard-canto/dashboard-canto.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { ComponentsModule } from '../../components/components.module';
 
 @Component({
   selector: 'app-dashboard-canto',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ComponentsModule],
   templateUrl: './dashboard-canto.component.html',
   styleUrl: './dashboard-canto.component.scss'
 })
