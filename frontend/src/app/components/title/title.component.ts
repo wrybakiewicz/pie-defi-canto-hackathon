5900273c8cf6d899525e50ae9ce15baaa753b47d
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 20:24:44 +0200

    Hide title on home page

--- frontend/src/app/components/title/title.component.ts
+++ frontend/src/app/components/title/title.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrl: './title.component.scss',
 })
-export class TitleComponent {}
+export class TitleComponent {
+
+  constructor(private router: Router) {}
+
+  isActive(url: string): boolean {
+    return this.router.isActive(url, {
+      paths: 'subset',
+      matrixParams: 'ignored',
+      queryParams: 'ignored',
+      fragment: 'ignored'
+    });
+  }
+}
