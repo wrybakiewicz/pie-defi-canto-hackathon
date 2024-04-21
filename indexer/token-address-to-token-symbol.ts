845cd92eb83443009497af73e481491fc9f78f57
Wojtek Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-04-21 21:28:15 +0200

    add atom

--- indexer/token-address-to-token-symbol.ts
+++ indexer/token-address-to-token-symbol.ts
@@ -3,6 +3,8 @@ export function getTokenSymbol(tokenAddress: string): string {
     return "WCANTO";
   } else if (tokenAddress === "0x5FD55A1B9FC24967C4dB09C513C3BA0DFa7FF687") {
     return "ETH";
+  } else if (tokenAddress === "0xecEEEfCEE421D8062EF8d6b4D814efe4dc898265") {
+    return "ATOM";
   }
   return "unknownToken";
 }
