a6c80ad1ec107ee6497fa15a11a0f2a1db965937
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 13:27:23 +0200

    Move title

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, ComponentsModule],
 })
 export class AppComponent {
   title = 'canto-web-ui';
