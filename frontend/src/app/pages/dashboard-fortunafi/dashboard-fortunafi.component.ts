b63a8df20e3a69419c2108b71c34c7baf8c07dd8
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 16:53:53 +0200

    Add fortunafi

--- frontend/src/app/pages/dashboard-fortunafi/dashboard-fortunafi.component.ts
+++ frontend/src/app/pages/dashboard-fortunafi/dashboard-fortunafi.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { ComponentsModule } from '../../components/components.module';
 
 @Component({
   selector: 'app-dashboard-fortunafi',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ComponentsModule],
   templateUrl: './dashboard-fortunafi.component.html',
   styleUrl: './dashboard-fortunafi.component.scss'
 })
