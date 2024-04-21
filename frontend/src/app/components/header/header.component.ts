7c8c5e82f7e34926b4415f3a4ace6b7f0beeea2c
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-21 12:33:11 +0200

    Add working redirects

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule], // Include CommonModule here
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private router: Router) { }
+
+  isActive(url: string): boolean {
+    // Check if the current route's URL matches the given URL
+    return this.router.isActive(url, {
+      paths: 'subset', // This can be 'exact' for exact matching or 'subset' for partial matching
+      matrixParams: 'ignored',
+      queryParams: 'ignored',
+      fragment: 'ignored'
+    });
+  }
+}
