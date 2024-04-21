33a0e9b368f11ecde2a64f391094233f3e634212
Wojtek Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-04-21 22:37:23 +0200

    query from lambda

--- api-lambda/index.js
+++ api-lambda/index.js
@@ -1,5 +1,36 @@
-exports.handler = async function (event, context) {
-  console.log("Hello world");
-};
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-// this.handler();
+const client = new DynamoDBClient({ region: "eu-west-2" });
+const docClient = DynamoDBDocumentClient.from(client, {
+  marshallOptions: {
+    removeUndefinedValues: true,
+  },
+});
+
+export async function handler(event, context) {
+  const address = event.queryStringParameters.address.toLowerCase();
+  console.log(address);
+
+  const getPositionsQuery = new QueryCommand({
+    TableName:
+      process.env.DYNAMODB_POSITIONS_FROM_TABLE_NAME ||
+      "piedefi-positions-from-v1",
+    KeyConditionExpression: "#key = :key",
+    ExpressionAttributeNames: { "#key": "account" },
+    ExpressionAttributeValues: { ":key": address },
+  });
+  const result = (await docClient.send(getPositionsQuery)).Items;
+  console.log(result);
+  return result;
+}
+
+// handler({
+//   queryStringParameters: {
+//     address: "0x1b58d50afd08dce062fb14d4e1f9665eb1eabeaf",
+//   },
+// });
