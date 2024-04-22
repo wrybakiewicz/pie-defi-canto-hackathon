8d887f708352bca87e6d2692fbca87dea4f5f242
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-22 15:28:36 +0200

    Add html

--- frontend/src/app/components/winrate/winrate.component.ts
+++ frontend/src/app/components/winrate/winrate.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-winrate',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class WinrateComponent {
 
+  @Input() styleOverride: string = '';
 }
