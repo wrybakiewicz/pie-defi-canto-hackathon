77464db0d0f8371ae16e7702bc26843987ee233d
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-22 17:24:05 +0200

    Add all trades progressbar

--- frontend/src/app/components/total-trades/total-trades.component.ts
+++ frontend/src/app/components/total-trades/total-trades.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-total-trades',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './total-trades.component.scss'
 })
 export class TotalTradesComponent {
+  @Input() styleOverride: string = '';
 
 }
