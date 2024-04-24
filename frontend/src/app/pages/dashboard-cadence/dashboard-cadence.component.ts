89ffba46c19f662080ddae452ce7bd9980c9fc93
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-24 15:21:03 +0200

    Fix example overlay

--- frontend/src/app/pages/dashboard-cadence/dashboard-cadence.component.ts
+++ frontend/src/app/pages/dashboard-cadence/dashboard-cadence.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ComponentsModule } from '../../components/components.module';
 import { HeaderComponent } from '../../components/header/header.component';
-import { CadenceData } from '../../models/cadence.model';
+import { CadenceData, PnlChart } from '../../models/cadence.model';
 import { MockDataService } from '../../services/mock-data.service';
 import { DashboardCadenceExampleComponent } from '../dashboard-cadence-example/dashboard-cadence-example.component';
 import { CommonModule } from '@angular/common';
+import { Subscription, Observable, Subject, timer, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-cadence',
@@ -13,14 +14,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard-cadence.component.html',
   styleUrl: './dashboard-cadence.component.scss',
 })
-export class DashboardCadenceComponent implements OnInit {
+export class DashboardCadenceComponent implements OnInit, AfterViewInit {
+  // Show example for testing purposes
+  private readonly showExample = true;
+
+  private subscription: Subscription = new Subscription();
+
   data!: CadenceData;
+  data$!: Observable<CadenceData>;
+  private pnlData = new Subject<PnlChart>();
+  pnlData$ = this.pnlData.asObservable();
 
-  constructor(private mockData: MockDataService) {}
-
+  constructor(private mockData: MockDataService) {
+    this.data$ = mockData.data$;
+  }
+  
   ngOnInit(): void {
-    // this.mockData.getCadenceDashboardData().subscribe((data) => {
-    //   // this.data = data;
-    // });
+    if(!this.showExample){
+      this.subscription.add(this.data$.subscribe((data) => {
+        this.data = data;
+        this.pnlData.next(data.pnlChart)
+      }));
+      this.mockData.getCadenceDashboardData()
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if(!this.showExample){
+      this.mockData.getCadenceDashboardData()
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
