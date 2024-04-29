872445a2838e099a5816a84b1060d11e447e8ae6
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-04-29 19:58:51 +0200

    Add button working

--- frontend/src/app/components/cadence-leaderboard/cadence-leaderboard.component.ts
+++ frontend/src/app/components/cadence-leaderboard/cadence-leaderboard.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { FeaturedTrade } from '../../models/featured-trades.mode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadence-leaderboard',
   templateUrl: './cadence-leaderboard.component.html',
   styleUrl: './cadence-leaderboard.component.scss',
 })
-export class CadenceLeaderboardComponent implements OnInit{
-
+export class CadenceLeaderboardComponent implements OnInit {
   topTrades: FeaturedTrade[] = [];
   worstTrades: FeaturedTrade[] = [];
   allTradesCount: number = 0;
 
-  constructor(private api: ApiService) { }
+  constructor(private router: Router, private api: ApiService) {}
 
   ngOnInit(): void {
     this.api.getFeaturedTrades().subscribe((trades) => {
-      if(trades.length < 5){
+      if (trades.length < 5) {
         this.topTrades = trades;
         this.allTradesCount = trades.length;
         return;
       }
-      this.topTrades = trades.slice(0,5);
+      this.topTrades = trades.slice(0, 5);
       this.worstTrades = trades.slice(-5);
-      this.allTradesCount = trades.length
+      this.allTradesCount = trades.length;
     });
+  }
+
+  onTradeSelected(address: string) {
+    this.router
+      .navigateByUrl(`/dashboard-cadence?address=${address}`)
+      .then(() => {
+        window.location.reload();
+      });
   }
 }
